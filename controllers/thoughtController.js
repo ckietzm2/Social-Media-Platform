004a28cc880c141b4d87217c0dddb49e038feeb1
Cody K <ckietzm2@gmail.com>
2023-11-15 09:50:46 -0600

    All Thought routes are working

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -25,14 +25,19 @@ module.exports = {
   // create a new video
   async createThought(req, res) {
     try {
-  
-      const thought = await Thought.create(
-        { $addToSet: { thoughts: thought._id } },
+      const dbThoughtData = await Thought.create(req.body);
+
+      const dbUserData = await User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $push: { thoughts: dbThoughtData._id } },
         { new: true }
       );
 
+      if (!dbUserData) {
+        return res.status(404).json({ message: 'Thought created but no user with this id!' });
+      }
 
-      res.json('Created the thought 🎉');
+      res.json({ message: 'Thought successfully created!' });
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
